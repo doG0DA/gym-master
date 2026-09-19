ef22684e8f691772e0d90dc58cd3471763d2b906
doG0DA <rusty20169@yandex.ru>
2026-09-19 22:35:47 +0300

    Реализовать Timetable

---
+++ src/ru/yande/practicum/Coach.java
@@ -0,0 +1,54 @@
+package ru.yande.practicum;
+
+import java.util.Objects;
+
+public class Coach {
+
+    //фамилия
+    private String surname;
+    //имя
+    private String name;
+    //отчество
+    private String middleName;
+
+    public Coach(String surname, String name, String middleName) {
+        this.surname = surname;
+        this.name = name;
+        this.middleName = middleName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    @Override
+    public String toString() {
+        return getSurname() + " " + getName() + " " + getMiddleName();
+    }
+
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || o.getClass() != getClass()) return false;
+        Coach coach = (Coach) o;
+
+        return Objects.equals(surname, coach.surname)
+                && Objects.equals(name, coach.name)
+                && Objects.equals(middleName, coach.middleName);
+
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(surname,name,middleName);
+    }
+}
---
+++ src/ru/yande/practicum/TimeTable.java
@@ -0,0 +1,74 @@
+package ru.yande.practicum;
+
+import java.util.*;
+
+public class TimeTable {
+
+    private Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
+
+    public void addNewTrainingSession(TrainingSession trainingSession) {
+        DayOfWeek day = trainingSession.getDayOfWeek();
+        TimeOfDay time = trainingSession.getTimeOfDay();
+
+        TreeMap<TimeOfDay, List<TrainingSession>> dayOfSession = timetable.get(day);
+        if (dayOfSession == null) {
+            dayOfSession = new TreeMap<>();
+            timetable.put(day, dayOfSession);
+        }
+
+        List<TrainingSession> listOfSession = dayOfSession.get(time);
+        if (listOfSession == null) {
+            listOfSession = new ArrayList<>();
+            dayOfSession.put(time, listOfSession);
+        }
+
+        listOfSession.add(trainingSession);
+    }
+
+    public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        TreeMap<TimeOfDay, List<TrainingSession>> daySchedule = timetable.get(dayOfWeek);
+        if (daySchedule == null) {
+            return new ArrayList<>();
+        }
+        List<TrainingSession> listOfTrainings = new ArrayList<>();
+        for (List<TrainingSession> session : daySchedule.values()) {
+            listOfTrainings.addAll(session);
+        }
+        return listOfTrainings;
+    }
+
+
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+        TreeMap<TimeOfDay, List<TrainingSession>> schedule = timetable.get(dayOfWeek);
+        if (schedule == null) {
+            return new ArrayList<>();
+        }
+        List<TrainingSession> trainingSessions = schedule.get(timeOfDay);
+        if (trainingSessions == null) {
+            return new ArrayList<>();
+        }
+
+        return trainingSessions;
+
+    }
+
+    public TreeMap<Coach, Integer> getCountByCoaches() {
+        Map<Coach, Integer> listOfTrainingsByCoach = new HashMap<>();
+
+        for (TreeMap<TimeOfDay, List<TrainingSession>> grid : timetable.values()) {
+            for (List<TrainingSession> listOfTrainings : grid.values()) {
+                for (TrainingSession session : listOfTrainings) {
+                    Coach coach = session.getCoach();
+                    listOfTrainingsByCoach.put(coach, listOfTrainingsByCoach.getOrDefault(coach, 0) + 1);
+                }
+            }
+        }
+
+        TreeMap<Coach, Integer> sorted = new TreeMap<>(new CoachCountComparator(listOfTrainingsByCoach));
+        sorted.putAll(listOfTrainingsByCoach);
+
+        return sorted;
+    }
+
+}
